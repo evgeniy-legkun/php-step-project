ad37defdafb752eedee037dafaaf987d89e3dbce
Evgeniy Legkun <evgeniy.legkun@gmail.com>
2018-09-16 16:15:27 +0300

    added gutter for control block

--- src/components/Authorisation.vue
+++ src/components/Authorisation.vue
@@ -1,76 +1,56 @@
 <template>
-  <StyledContentBlock>
-    <StyledRegistrationBlock>
-      <div>
-        <BootstrapImage
-            :src="require('@/assets/img/avatar.jpg')"
-            width="85"
-            height="85"
-            rounded="circle"
-        />
-      </div>
-      <div>
-        User name :
-        <BootstrapInput
-            v-model="formData.userName"
-            name="form_user_name"
-            v-validate="'required'"
-        />
-        <span v-show="errors.has('form_user_name')" style="color: red;">
+  <ControlGutter>
+    <div>
+      <BootstrapImage
+          :src="require('@/assets/img/avatar.jpg')"
+          width="85"
+          height="85"
+          rounded="circle"
+      />
+    </div>
+    <div>
+      User name :
+      <BootstrapInput
+          v-model="formData.userName"
+          name="form_user_name"
+          v-validate="'required'"
+      />
+      <span v-show="errors.has('form_user_name')" style="color: red;">
           The field is required<br>
         </span>
-        Password :
-        <BootstrapInput
-            v-model="formData.password"
-            name="form_password"
-            type="password"
-            v-validate="'min:6|required'"
-        />
-        <span v-show="errors.has('form_password')" style="color: red;">
+      Password :
+      <BootstrapInput
+          v-model="formData.password"
+          name="form_password"
+          type="password"
+          v-validate="'min:6|required'"
+      />
+      <span v-show="errors.has('form_password')" style="color: red;">
           Password less than 6 characters<br>
         </span>
-      </div>
-      <div>
-        <BootstrapButton variant="success" @click="authUser">
-          Authorisation
-        </BootstrapButton>
-      </div>
-    </StyledRegistrationBlock>
-  </StyledContentBlock>
+    </div>
+    <div>
+      <BootstrapButton variant="success" @click="authUser">
+        Authorisation
+      </BootstrapButton>
+    </div>
+  </ControlGutter>
 </template>
 
 <script>
-  import styled from 'vue-styled-components';
   import BootstrapImage from 'bootstrap-vue/es/components/image/img';
   import BootstrapInput from 'bootstrap-vue/es/components/form-input/form-input';
   import BootstrapButton from 'bootstrap-vue/es/components/button/button';
-
-  const StyledContentBlock = styled('div')`
-      display: grid;
-      grid-template-rows: 1fr;
-      justify-items: center;
-    `;
-
-  const StyledRegistrationBlock = styled('div')`
-      display: grid;
-      grid-template-columns: 1fr;
-      grid-template-rows: 1fr 2fr 1fr;
-      border-radius: 8px;
-      width: 300px;
-      border: 1px solid blue;
-      grid-row-gap: 25px;
-      padding: 15px;
-    `;
+  import ControlGutter from './ControlGutter';
 
   export default {
     name: 'Authorisation',
 
     components: {
-      StyledRegistrationBlock,
-      StyledContentBlock,
       BootstrapImage,
       BootstrapInput,
-      BootstrapButton
+      BootstrapButton,
+      ControlGutter
     },
 
     data () {
---
+++ src/components/ControlGutter.vue
@@ -0,0 +1,37 @@
+<template>
+  <StyledContentBlock>
+    <StyledFormBlock>
+      <slot></slot>
+    </StyledFormBlock>
+  </StyledContentBlock>
+</template>
+
+<script>
+  import styled from 'vue-styled-components';
+
+  const StyledContentBlock = styled('div')`
+      display: grid;
+      grid-template-rows: 1fr;
+      justify-items: center;
+    `;
+
+  const StyledFormBlock = styled('div')`
+      display: grid;
+      grid-template-columns: 1fr;
+      grid-template-rows: 1fr 2fr 1fr;
+      border-radius: 8px;
+      width: 300px;
+      border: 1px solid blue;
+      grid-row-gap: 25px;
+      padding: 15px;
+    `;
+
+  export default {
+    name: 'ControlGutter',
+
+    components: {
+      StyledContentBlock,
+      StyledFormBlock
+    }
+  };
+</script>
